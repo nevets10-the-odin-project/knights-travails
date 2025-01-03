3f1aea22a3df8b59d8124e4d4e53482d4bda708c
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-02 19:37:21 -0800

    Add cur_pos to knight class
    I think I'll want to keep track of the original starting position so I
    can check if it comes up as a move. Then I can ignore it.

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,12 +1,13 @@
 require_relative 'node'
 
 class Knight
-  attr_accessor :start_pos, :end_pos
+  attr_accessor :start_pos, :cur_pos, :end_pos
 
   MOVES = [[2, 1], [1, 2], [-2, 1], [1, -2], [2, -1], [-1, 2], [-2, -1], [-1, -2]]
 
   def initialize(start_pos, end_pos)
-    @start_pos = build_tree(start_pos)
+    @start_pos = start_pos
+    @cur_pos = build_tree(start_pos)
     @and_pos = end_pos
   end
 
@@ -19,4 +20,6 @@ class Knight
     potential = MOVES.map { |move| [move[0] + root[0], move[1] + root[1]] }
     potential.filter { |move| move[0].between?(0, 7) && move[1].between?(0, 7) }
   end
+
+  # Build method to traverse through nodes
 end
