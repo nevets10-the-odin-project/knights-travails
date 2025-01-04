d504a1581fff178d04f2e74d86a8942d725c753d
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-03 18:26:38 -0800

    Remove start_pos, correct end_pos spelling

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,14 +1,13 @@
 require_relative 'node'
 
 class Knight
-  attr_accessor :start_pos, :cur_pos, :end_pos
+  attr_accessor :cur_pos, :end_pos
 
   MOVES = [[2, 1], [1, 2], [-2, 1], [1, -2], [2, -1], [-1, 2], [-2, -1], [-1, -2]]
 
   def initialize(start_pos, end_pos)
-    @start_pos = start_pos
     @cur_pos = build_tree(start_pos)
-    @and_pos = end_pos
+    @end_pos = end_pos
   end
 
   def build_tree(root)
@@ -21,5 +20,7 @@ class Knight
     potential.filter { |move| move[0].between?(0, 7) && move[1].between?(0, 7) }
   end
 
-  # Build method to traverse through nodes
+  # Build method to traverse through nodes.
+  # The tree should grow with each move to take
+  # them out of the potential list of future moves
 end
