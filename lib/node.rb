95260e5a0ce98a9a8ffb685612b99c9271252a1f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-03 19:06:06 -0800

    Add prev_root to node
    I was reading ahead in the Intermediate Git lessons and the explanation
    of how commits and pointers work reminded me of a linked list, but
    backwards. Commits point to their parent instead of vice versa.
    I think that could work here with tracing back the steps it took to get
    to the end position. This isn't a binary tree, so I can't start from the
    root and go down very easily.

--- lib/node.rb
+++ lib/node.rb
@@ -1,11 +1,12 @@
 class Node
   include Comparable
 
-  attr_accessor :root, :children
+  attr_accessor :root, :children, :prev_root
 
-  def initialize(root, moves = [])
+  def initialize(root, moves = [], prev_root = nil)
     @root = root
     @children = moves
+    @prev_root = prev_root
   end
 
   def <=>(other)
